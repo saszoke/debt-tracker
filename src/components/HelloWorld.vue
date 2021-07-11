7f645f31b3931daa5a17ab024e3ff23bc07e5c38
Sándor Szőke <SandorS@topdesk.com>
2021-07-11 19:26:44 +0200

    carousel complete?!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,76 +1,62 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+  <div :class="width">
+    <v-carousel v-model="model" :show-arrows="false" hide-delimiters height="auto">
+      <v-img
+        contain
+        src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+        class="white--text align-end"
+        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+      
+      >
+      </v-img> <!--CHECK CARD REVEAL INSTEAD-->
+      <v-carousel-item v-for="(color, i) in mylist" :key="color">
+        <v-sheet :color="color">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2 py-10">Slide {{ i + 1 }}</div>
+            <v-expansion-panels class="">
+              <v-expansion-panel v-for="debt in debts" :key="debt.id">
+                <v-expansion-panel-header class="pa-5 grey--text" color="light-green lighten-5">
+                  <div class="d-flex justify-space-between pa-2">
+                    <div class="mx-5 green--text font-weight-bold">{{ debt.amount }} HUF</div>
+                    <div class="mx-5">{{ debt.date }}</div>
+                  </div>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  
+                  <v-btn color="warning" dark class="ma-5">
+                    <span class="mr-2">Payback (incomplete)</span>
+                    <v-icon>mdi-cash</v-icon>
+                  </v-btn>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to the beautiful Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+                  <v-btn color="success" dark class="ma-5">
+                    <span class="mr-2">Complete Payback</span>
+                    <v-icon>mdi-cash-check</v-icon>
+                  </v-btn>
+                  <v-tooltip
+                    v-model="show"
+                    top
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        icon
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <v-icon color="grey lighten-1">
+                          mdi-information
+                        </v-icon>
+                      </v-btn>
+                    </template>
+                    <span>{{debt.info}}</span>
+                  </v-tooltip>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
@@ -78,56 +64,39 @@ export default {
   name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
+    mylist: ["light-green lighten-1", "light-green lighten-2", "light-green lighten-3", "light-green lighten-4", "light-green lighten-5"],
+
+    debts: [
+      { id: 1, date: "03/25/2015", amount: 10000, info: "Random information about debt" },
+      { id: 2, date: "03/25/2015", amount: 10000, info: "Random information about debt" },
+      { id: 3, date: "03/25/2015", amount: 10000, info: "Random information about debt" },
+      { id: 4, date: "03/25/2015", amount: 10000, info: "Random information about debt" },
+      { id: 5, date: "03/25/2015", amount: 10000, info: "Random information about debt" },
     ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer",
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
+    model: 0,
+
   }),
+  computed: {
+    width () {
+        switch (this.$vuetify.breakpoint.name) { //$vuetify.breakpoint.xs ? '' : 'too-big'
+          case 'xs': return ""
+          case 'sm': return ""
+          case 'md': return "too-big"
+          case 'lg': return "too-big"
+          case 'xl': return "too-big"
+        }
+        return ""
+      }
+  },
+
+  watch: {},
 };
 </script>
+
+<style>
+.too-big{
+  max-width: 50%;
+  margin: 0 auto;
+  border: 1px solid grey;
+}
+</style>
