80cfe0a0bc9e8ee1212fea508648266567aeb5ff
Sándor Szőke <SandorS@topdesk.com>
2021-07-16 22:25:54 +0200

    login & register

--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,19 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/login",
+    name: "Login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
   },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/Register.vue")
+  }
 ];
 
 const router = new VueRouter({
